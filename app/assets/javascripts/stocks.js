c28c1a5e1c0e3897d7339df7a921cdf1bc385aa2
Sumit Gautam <sghost33@gmail.com>
2016-08-04 08:05:07 +0000

    updating the my portfolio page and handling requirest with ajax

---
+++ app/assets/javascripts/stocks.js
@@ -0,0 +1,25 @@
+var init_stock_lookup;
+
+
+init_stock_lookup = function(){
+  $('#stock-lookup-form').on('ajax:success', function(event, data, status){
+    $('#stock-lookup').replacewith(data);
+    init_stock_lookup();
+  })
+
+}
+
+
+
+
+
+
+
+
+
+
+$(document).ready(function (){
+  init_stock_lookup();
+})
+
+})
